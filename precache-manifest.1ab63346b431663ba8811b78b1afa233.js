441c6a47b7f310005dd62c6f5e47ebdc1205c40e
AleksandrKaradzikov <karadzikovaleksandr@gmail.com>
2020-03-31 12:33:26 +0300

    Updates

---
+++ precache-manifest.1ab63346b431663ba8811b78b1afa233.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "856bfae1513cdbe7233d8b68ede59be3",
+    "url": "/PERCo-photo-gallary-test-item/index.html"
+  },
+  {
+    "revision": "84cd4d33b17346496850",
+    "url": "/PERCo-photo-gallary-test-item/static/css/2.ba7672f6.chunk.css"
+  },
+  {
+    "revision": "9460505c7fbf288ae64c",
+    "url": "/PERCo-photo-gallary-test-item/static/css/main.d2d42a77.chunk.css"
+  },
+  {
+    "revision": "84cd4d33b17346496850",
+    "url": "/PERCo-photo-gallary-test-item/static/js/2.d64da8dc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/PERCo-photo-gallary-test-item/static/js/2.d64da8dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9460505c7fbf288ae64c",
+    "url": "/PERCo-photo-gallary-test-item/static/js/main.4af28d4c.chunk.js"
+  },
+  {
+    "revision": "de98b5371667fd642819",
+    "url": "/PERCo-photo-gallary-test-item/static/js/runtime-main.1835c48e.js"
+  }
+]);
